17aef91baf238e0684d72929e28e4279af27cddb
Sergii K <sergiik@lcgroups.com>
2022-12-05 01:12:26 +0200

    [GM] - add 'Open in new tab' for tracks list

--- src/app/routes/open-playlist/open-playlist.module.ts
+++ src/app/routes/open-playlist/open-playlist.module.ts
@@ -4,19 +4,17 @@ import { OpenPlaylistComponent } from './open-playlist.component';
 import { RouterModule } from '@angular/router';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { AudioModule } from 'src/app/components/audio/audio.module';
+import { TooltipModule } from 'primeng/tooltip';
 
 @NgModule({
-  declarations: [
-    OpenPlaylistComponent
-  ],
+  declarations: [OpenPlaylistComponent],
   imports: [
     CommonModule,
     ProgressSpinnerModule,
     AudioModule,
+    TooltipModule,
     RouterModule.forChild([{ path: '', component: OpenPlaylistComponent }]),
   ],
-  exports: [
-    OpenPlaylistComponent
-  ]
+  exports: [OpenPlaylistComponent],
 })
-export class OpenPlaylistModule { }
+export class OpenPlaylistModule {}
